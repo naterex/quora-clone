60a7d79e7410f51581a41327a570e157ff3958dc
Nate <Welling>
2016-02-12 14:23:19 +0800

    add user model with bcrypt, add signup.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
-require 'bcrypt'
-
 class User < ActiveRecord::Base
+  has_secure_password
+
   # users.password_hash in the database is a :string
-  include BCrypt
+  attr_accessor :password, :password_confirmation
 
   def password
     @password ||= Password.new(password_hash)
@@ -12,4 +12,29 @@ class User < ActiveRecord::Base
     @password = Password.create(new_password)
     self.password_hash = @password
   end
+
+  # assign them a random one and mail it to them, asking them to change it
+  def forgot_password
+    @user = User.find_by_email(params[:email])
+    random_password = Array.new(10).map { (65 + rand(58)).chr }.join
+    @user.password = random_password
+    @user.save!
+    Mailer.create_and_deliver_password_change(@user, random_password)
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.password = params[:password]
+    @user.save!
+  end
+
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      give_token
+    else
+      redirect_to home_url
+    end
+  end
+
 end
